66069e9e98dff2e3e174d9c0a108d8f3e1a60044
열일 <isawthat77@naver.com>
2022-08-07 21:50:30 +0900

    Union 타입

---
+++ 01. 기본 타입 마스터 하기/1-5-Union.ts
@@ -0,0 +1,42 @@
+{
+    /**
+     * Union Types : OR
+     */
+    type Direction = 'left' | 'right' | 'up' | 'down';
+    function move(direction : Direction){
+        console.log(direction);
+    }
+    move('down')
+
+    type TileSize = 8 | 16 | 32;
+    const title: TileSize = 16;
+
+    // function: login -> success, fail
+    type SuccessState={
+        response:{
+            body: string;
+        }
+    }
+    type FailState = {
+        reason: string;
+    }
+    type LoginState = SuccessState | FailState
+    function login(id: string, password: string): LoginState{
+        return {
+            response:{
+                body:'logged in'
+            }
+        }
+    }
+
+    // printLoginState(state) 
+    // success -> response body 출력
+    // fail -> reason 출력
+    function printLoginState(state: LoginState){
+        if('response' in state){
+            console.log(state.response.body)
+        }else{
+            console.log(state.reason)
+        }
+    }
+}
