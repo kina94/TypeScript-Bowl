ce55bb1c097c686302e70dda60b784c796e6d1c1
열일 <isawthat77@naver.com>
2022-08-08 16:01:36 +0900

    객체지향적으로 커피머신 수정하기

--- 03. 객체지향 프로그래밍/3-2-coffee-machine.ts
+++ 03. 객체지향 프로그래밍/3-2-coffee-machine.ts
@@ -3,22 +3,34 @@
         shots: number;
         hasMilk : boolean;
     };
-    
-    const BEANS_GRAMM_PER_SHOT = 7; // 타입 추론
-    let coffeBeansGramm: number = 0;
 
-    function makeCoffe(shots: number): CoffeCup {
-        if(coffeBeansGramm < shots * BEANS_GRAMM_PER_SHOT){
-            throw new Error('Not enough coffee beans!')
+    class CoffeMaker { //서로 관련 있는 데이터와 함수를 묶는 템플릿
+        static BEANS_GRAMM_PER_SHOT = 7; // class level : 오브젝트마다 생성되지 않음. (함께 공유돨 수 있는 변수에 사용)
+        coffeBeansGramm: number = 0; // instance (object) level : 오브젝트마다 생성.
+
+        constructor(coffeBeansGramm: number){ // instance를 만들 때 호출되는 함수
+            this.coffeBeansGramm = coffeBeansGramm
         }
-        coffeBeansGramm -= shots * BEANS_GRAMM_PER_SHOT
-        return {
-            shots,
-            hasMilk: false,
+
+        static makeMachine(coffeBeansGram: number): CoffeMaker {
+            return new CoffeMaker(coffeBeansGram)
+        }
+
+        makeCoffe(shots: number): CoffeCup {
+            // 클래스 변수에 접근할 때는 this를 사용
+            if(this.coffeBeansGramm < shots * CoffeMaker.BEANS_GRAMM_PER_SHOT){
+                throw new Error('Not enough coffee beans!')
+            }
+            this.coffeBeansGramm -= shots * CoffeMaker.BEANS_GRAMM_PER_SHOT
+            return {
+                shots,
+                hasMilk: false,
+            }
         }
     }
 
-    coffeBeansGramm += 3 * BEANS_GRAMM_PER_SHOT;
-    const coffee = makeCoffe(2)
-    console.log(coffee)
+    const maker = new CoffeMaker(32);
+    console.log(maker)
+    const maker2 = CoffeMaker.makeMachine(3);
+    console.log(maker2)
 }
