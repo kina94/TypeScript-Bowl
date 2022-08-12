41a97eab3e11a0ffddeaae876ded964b614619b3
열일 <isawthat77@naver.com>
2022-08-13 00:25:26 +0900

    Type Alias와 Interface의 개념 측면의 차이점

--- 07. 타입 스크립트의 핵심/9-1-interface.ts
+++ 07. 타입 스크립트의 핵심/9-1-interface.ts
@@ -48,4 +48,11 @@
   type Name = Person['name']; //string
   type NumberType = number;
   type Direction = 'left' | 'right'
+
+
+  /**
+   * interfcae는 '명세서'와 같은 역할을 하기 때문에 '규격'이 필요한 곳에 사용되어야 한다.
+   * (API 등, class 등)
+   * Type Alias는 어떤 것을 구현할 목적이 아니라 데이터를 담을 목적일 때 사용한다.
+   */
 }
