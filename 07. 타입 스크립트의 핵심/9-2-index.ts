85d3b51eafe357319c4ff3677dcdd55121db7102
열일 <isawthat77@naver.com>
2022-08-13 00:33:58 +0900

    인덱스 타입

---
+++ 07. 타입 스크립트의 핵심/9-2-index.ts
@@ -0,0 +1,34 @@
+{
+    const obj = {
+        name: 'kina'
+    }
+
+    obj.name; //kina
+    obj['name'] //ellie
+
+    type Animal = {
+        name: string;
+        age: number;
+        gender: 'male' | 'female';
+    };
+
+    type Name = Animal['name'] // string
+    const text : Name = 'hello'
+
+    type Gender = Animal['gender']; //'male' | 'female'
+
+    type Keys = keyof Animal // 'name' | 'age' | 'gender'
+
+    const key: Keys = 'gender'
+
+    type Person = {
+        name: string;
+        gender: Animal['gender']
+    }
+
+    const person: Person = {
+        name: 'kina',
+        gender: 'male'
+    }
+
+}
