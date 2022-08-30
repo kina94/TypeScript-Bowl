4579e56016d425c64dc4432b1fa0f1c83cdf32e8
kina <chase245@naver.com>
2022-08-30 23:18:06 +0900

    모듈화

---
+++ 08. 자바스크립트 프로토타입/9-4-module.js
@@ -0,0 +1,4 @@
+export default function add(num1, num2) {
+  // default로 export하면 이름을 자유롭게 지정할 수 있음
+  return num1 + num2;
+}
---
+++ 08. 자바스크립트 프로토타입/9-5-module2.js
@@ -0,0 +1,5 @@
+import add from "./9-4-module";
+console.log(add(1, 2));
+function add(a, b) {
+  return a * b;
+}
